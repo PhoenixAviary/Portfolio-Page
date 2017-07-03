896280e502f8b9e4cd686632fe136fcff8d34cd6
PhoenixAviary <valerieannphoenix@gmail.com>
2017-07-03 19:07:55 -0400

    Add files via upload

---
+++ album/album.js
@@ -0,0 +1,27 @@
+/*
+  version: BananAlbum $bananalbum
+  generator: jAlbum 14
+*/
+
+album({
+  title: 'Bioluminescence Web',
+  description: '',
+
+  bounds: [
+    {width: 480, height: 360, path: 'thumbs/'},
+    {width: 1640, height: 1460, path: 'slides/'},
+    {width: 2048, height: 2048, path: 'hi-res/'}
+  ],
+  
+  slides: [
+    {image: 'Bioluminescence_Final-1.jpg', width: 937, height: 1400, title: '', description: ''},
+    {image: 'Bioluminescence_Final-2.jpg', width: 1400, height: 1024, title: '', description: ''},
+    {image: 'Bioluminescence_Final-3.jpg', width: 937, height: 1400, title: '', description: ''},
+    {image: 'Bioluminescence_Final-4.jpg', width: 934, height: 1400, title: '', description: ''},
+    {image: 'Bioluminescence_Final-5.jpg', width: 937, height: 1400, title: '', description: ''},
+    {image: 'Bioluminescence_Final-6.jpg', width: 1400, height: 1132, title: '', description: ''},
+    {image: 'Bioluminescence_Final-7.jpg', width: 1400, height: 937, title: '', description: ''},
+    {image: 'Bioluminescence_Final-8.jpg', width: 925, height: 1400, title: '', description: ''},
+    {image: 'Bioluminescence_Final-9.jpg', width: 1400, height: 937, title: '', description: ''}
+  ]
+});
---
+++ album/menu.js
@@ -0,0 +1,17 @@
+/*
+  version: BananAlbum $bananalbum
+  generator: jAlbum 14
+*/
+
+menu([
+
+{
+  title: 'Bioluminescence Web',
+  description: '',
+  path: '',
+  total: 9,
+  space: true
+},
+
+]);
+
